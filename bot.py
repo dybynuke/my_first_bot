b5377ea2302cc626c3394d9ac15585b3263bceb0
dybynuke <56640477+dybynuke@users.noreply.github.com>
2020-09-15 14:10:07 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,13 @@ from telebot import types
 
 a = "[14:00 - 15:50] Интеллектульные информационные системы и базы знаний(л)\nhttps://us04web.zoom.us/j/6467674915?pwd=SmNYaUlBMGVTMlJ2ZVdsOTgvcG1yZz09\n[16:00 - 18:50] Кроссплатформенная разработка мобильных приложений (Б)\nhttps://us04web.zoom.us/j/5149248980?pwd=UFVyd3FjVnNEQ3BURkJSU3FrdWJxUT09"
 
-b = "[11:00 - 13:50] Администрирование систем и сетей\nhttps://us04web.zoom.us/j/7621644476?pwd=d3daRUdJUml5WkFlcUErVytKZEczZz09"
+b = "[11:00 - 13:50] Администрирование систем и сетей\nhttps://us04web.zoom.us/j/7621644476?pwd=d3daRUdJUml5WkFlcUErVytKZEczZz09\n[14:00 - 15:50] Распределенные информационные системы\nhttps://us04web.zoom.us/j/5900790741?pwd=NkZrUFJ0QXFQVUpERjN2Z1IyN2FwZz09"
 
 c = "[10:00 - 11:50] Программирование в MS SQL (б)\nhttps://us04web.zoom.us/j/8636724102\n[12:00 - 14:50] Информационнные системы предприятия\nhttps://us04web.zoom.us/j/8635475529\n[15:00 - 16:50]Программирование в MS SQL (а)\nhttps://us04web.zoom.us/j/8636724102"
 
 d = "[8:00 - 9:50] Программирование в MS SQL (б)\nhttps://us04web.zoom.us/j/8636724102\n[10:00 - 11:50] Информационнные системы предприятия\nhttps://us04web.zoom.us/j/8635475529\n[12:00 - 13:50]Программирование в MS SQL (а)\nhttps://us04web.zoom.us/j/8636724102"
+
+e = "[пятница]"
 
 token = os.environ.get('bot_token');
 bot = telebot.TeleBot(str(token));
@@ -43,9 +45,9 @@ def start(message):
 	elif message.text == 'среда' or message.text == "Среда" or message.text == "ср" or message.text == "Ср":
 		bot.send_message(message.from_user.id, c)
 	elif message.text == 'пятница' or message.text == "Пятница" or message.text == "пт" or message.text == "Пт":
-		bot.send_message(message.from_user.id, d)
+		bot.send_message(message.from_user.id, e)
 	elif message.text == 'четверг' or message.text == "Четверг" or message.text == "чт" or message.text == "Чт":
-		bot.send_message(message.from_user.id, "Военная кафедра!")
+		bot.send_message(message.from_user.id, d)
 	elif message.text == 'суббота' or message.text == "Суббота" or message.text == "Воскресенье" or message.text == "воскресенье":
 		bot.send_message(message.from_user.id, "Выходной день! Отдыхай!")
 	else:
